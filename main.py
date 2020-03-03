a6ea615c620201746ef4d3dfd56737ad94ecdfce
loxygenk <loxygen.k@gmail.com>
2020-03-03 21:49:56 +0900

    †リファクタ† まさかそんなものがあるとは思わんやん?

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ client = discord.Client()
 token = sys.argv[1]
 
 github_cmd_regex = re.compile(r".*?\#(.+)\/([0-9]+)[^\s]*?.*?")
+channel_id_regex = re.compile(r"^<#([0-9]+?)>$")
 
 @client.event
 async def on_ready():
@@ -27,6 +28,9 @@ async def on_message(message):
         # matches[0]だとmatch関数にかけた文字列が全部返ってくる(なぜ)
         repo_name = matches[1]
         num = matches[2]
+
+        if channel_id_regex.match(repo_name) is not None:
+            repo_name = client.get_channel(int(channel_id_regex.match(repo_name)[1]))
 
         res = requests.get("https://github.com/brokenManager/{}/issues/{}".format(repo_name, num))
         if res.status_code == 404:
