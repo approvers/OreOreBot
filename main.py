efba00ef97d0ef3bb106bff541e259feef62de57
isso0424 <isso04240129@gmail.com>
2020-03-15 02:40:50 +0900

    †修正†結構class化した あとコマンドをclass化してしまえば勝ち

--- main.py
+++ main.py
@@ -11,38 +11,23 @@ from random import randint
 
 from lib import scraping
 from lib.weather import get_weather
+from lib.util import Singleton
+from lib.lol_counter import LolCounter
 
 import re
 
 CLIENT = discord.Client()
-HARASYO = CLIENT.get_emoji(684424533997912096)
 TOKEN = os.environ["TOKEN"]
 FIRST_CHANNEL = 684289417682223150
 ID_EXCEPTIONS = [685429240908218368, 684655652182032404, 685457071906619505]
 lol_count = {}
 typos = {}
 
-GITHUB_CMD_REGEX = re.compile(r".*?\#(.+?)\/([^\s]+).*?")
-CHANNEL_ID_REGEX = re.compile(r"^<#([0-9]+?)>$")
-USER_CMD_REGEX = re.compile(r"^!(\w+?)*(\s\w+)*")
-TYPO_REGEX = re.compile(r"^.*だカス$")
-
-JTC_TZ = datetime.timezone(datetime.timedelta(hours=9))
-
-try:
-    # mesm_json_syntax_conceal = 0sages.json (時報json) の読み込みを試みる
-    # msg_dictのkeyはstr型です、int型で呼び出そうとしないで()
-    with codecs.open("messages.json", 'r', 'utf-8') as f:
-        msg_dict = json.loads(f.read())
-except:
-    print("There is no messages.json\nPlease check your directory")
-
-
 
 @CLIENT.event
 async def on_ready():
     channel = CLIENT.get_channel(int(FIRST_CHANNEL))
-    await channel.send(msg_dict["login"])
+    await channel.send(msg_dict["base"])
     asyncio.ensure_future(ziho(channel))
 
 
@@ -64,8 +49,7 @@ async def ziho(channel):
             if hour == "6":
                 weather = get_weather()["today"]
                 await home_channel.send(
-                    "今日の天気は{}\n最高気温は{}℃で昨日と{}℃違うよ\n最低気温は{}℃で昨日と{}℃違うよ\n今日も頑張ってね"\
-                    .format(
+                    "今日の天気は{}\n最高気温は{}℃で昨日と{}℃違うよ\n最低気温は{}℃で昨日と{}℃違うよ\n今日も頑張ってね"\ .format(
                         weather["weather"],
                         weather["high"],
                         weather["high_diff"][1:-1],
@@ -272,3 +256,116 @@ async def on_message(message):
             await channel.send(get_message("bold-italic-cop", "message").format(HARASYO))
 
 CLIENT.run(TOKEN)
+
+class MainClient(discord.Client, Singleton):
+    """
+    Discordクライアント(多重起動防止機構付き)
+    """
+    CLI_BOTS = [
+        685429240908218368, 
+        684655652182032404, 
+        685457071906619505        
+    ]
+    def __init__(self, token, base_channel_id):
+        """
+        クライアントを起動する前の処理
+        tokenとか最初にメッセージ送信するチャンネルの指定をしたりする
+        Parameters
+        ----------
+        token: str
+            discordのBotのトークン
+        base_channel_id: int
+            ログインをし、時報を送信するチャンネルのid
+        """
+        super(MainClient, self).__init__()
+        self.token = token
+        self.base_channel_id = base_channel_id
+
+        # Initialize in on_ready()
+        # Because use value in client
+        self.base_channel = None
+        self.lol_counter  = None
+        self.HARASYO      = None
+
+        # mesm_json_syntax_conceal = 0sages.json (時報json) の読み込みを試みる
+        # msg_dictのkeyはstr型です、int型で呼び出そうとしないで()
+        with codecs.open("messages.json", 'r', 'utf-8') as f:
+            self.msg_dict = json.loads(f.read())
+
+
+    async def on_ready(self):
+        """
+        Clientの情報をもとにした初期化と時報の起動
+        """
+        if len(self.guilds) == 1:
+            self.base_channel = self.get_channel(self.base_channel_id)
+            self.lol_counter = LolCounter(self.guilds[0].members)
+            self.HARASYO = CLIENT.get_emoji(684424533997912096)
+
+    async def on_message(self, message):
+        if message.author.bot and not message.author.id in MainClient.CLI_BOTS:
+            return
+        channel = message.channel
+        message_str = message.content
+
+    async def ziho(self):
+        """
+        時報を制御する関数
+        """
+        timezone = datetime.timezone(datetime.timedelta(hours=9))
+        messages = self.msg_dict["ziho"]
+        while True:
+            time = datetime.datetime.now(tz=timezone)
+            if time.minute == 0:
+                hour = str(time.hour)
+                await self.base_channel.send(messages[hour])
+                if hour == "6":
+                    weather = get_weather()["today"]
+                    await home_channel.send(
+                        "今日の天気は{}\n最高気温は{}℃で昨日と{}℃違うよ\n最低気温は{}℃で昨日と{}℃違うよ\n今日も頑張ってね"\ .format(
+                            weather["weather"],
+                            weather["high"],
+                            weather["high_diff"][1:-1],
+                            weather["low"],
+                            weather["low_diff"][1:-1]
+                        )
+                    )
+            if hour == "19":
+                weather = get_weather()["tomorrow"]
+                await home_channel.send(
+                    "明日の天気は{}\n最高気温は{}℃で今日と{}℃違うよ\n最低気温は{}℃で今日と{}℃違うよ\n今日も1日お疲れ様"\
+                    .format(
+                        weather["weather"],
+                        weather["high"],
+                        weather["high_diff"][1:-1],
+                        weather["low"],
+                        weather["low_diff"][1:-1]
+                    )
+                )
+            await asyncio.sleep(15)
+        await asyncio.sleep(50)
+
+    def launch(self):
+        self.run(self.token)
+
+class MessageCommands:
+    REGEXES = {
+        "GitHub"      : re.compile(r".*?\#(.+?)\/([^\s]+).*?"),
+        "Channel"     : re.compile(r"^<#([0-9]+?)>$"),
+        "User Command": re.compile(r"^!(\w+?)*(\s\w+)*"),
+        "Typo"        : re.compile(r"^.*だカス$")
+    }
+
+    def __init__(self, message):
+        """
+        インスタンス化の際にメッセージだけ受け取る
+        message: str
+            メッセージの文章
+        """
+        self.message = message
+        for (key, regex) in MessageCommands.REGEXES.items():
+            if regex.match(message):
+                self.command_type = key
+        if self.command_type is None:
+            self.command_type = ""
+
