aa3aa6c73fa4e88cb80954325bf9ddd593ed36be
isso0424 <isso04240129@gmail.com>
2020-03-15 00:48:05 +0900

    †修正†lol_counterをシングルトンで実装+lib/util.pyにシングルトン用基底クラスを実装

---
+++ lib/lol_counter.py
@@ -0,0 +1,58 @@
+from util import *
+
+
+class LolCounter(Singleton):
+    """
+    「草」をカウントしてくれるクラス
+    """
+    _LOL_MESSAGE = {
+        "counter-value": "私が起きてから、司令官は{}回「草」って言ってるね",
+        "too-many"     : "...いくらなんでも多すぎないかい?",
+        "no-lol"       : "まだ「草」って言ってないみたいだね"
+    }
+    def __init__(self, members):
+        """
+        変数の初期化を行う
+        Parameters
+        ----------
+        members : List<discord.member>
+            サーバーのメンバーのリスト
+        """
+        self.lol_count = {}
+        for member in members:
+            self.lol_count[member.id] = 0
+
+    def count(self, message, author_id):
+        """
+        草を行った回数をlol_countに追加する
+        Parameters
+        ----------
+        message  : str
+            実際に発言されたメッセージ
+        author_id: int
+            発言者のid
+        """
+        if author_id in self.lol_count.keys():
+            self.lol_count[author_id] += message.count("草")
+            return
+        self.lol_count[author_id] = message.count("草")
+
+    def output(self, channel, author_id):
+        """
+        現在の草の数を出力する
+        channel: discord.Channel
+            コマンドを入力されたチャンネル
+        author_id: int
+            発言者のid
+        """
+        if author_id in self.lol_count.keys():
+            count = self.lol_count[author_id]
+            if count == 0:
+                await channel.send(LolCounter._LOL_MESSAGE["no-lol"])
+                return
+            await channel.send(LolCounter._LOL_MESSAGE["counter-value"].format(count))
+            if count >= 10:
+                await channel.send(LolCounter._LOL_MESSAGE["too-many"])
+            return
+        await channel.send(LolCounter._LOL_MESSAGE["no-lol"])
+
---
+++ lib/util.py
@@ -0,0 +1,9 @@
+class Singleton(object):
+    """
+    これを継承したclassはシングルトンになる
+    """
+    def __new__(cls, *_, **__):
+        if not hasattr(cls, "_instance"):
+            cls._instance = super(Singleton, cls).__new__(cls)
+        return cls._instance
+
