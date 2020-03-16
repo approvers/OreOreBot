b5a561ea6c3c01ced7186da977c0a9dcee82c8fc
isso0424 <isso04240129@gmail.com>
2020-03-16 16:15:51 +0900

    †修正†時報をファイル分けしてdocstringを書き始めた

---
+++ lib/time_signal.py
@@ -0,0 +1,102 @@
+"""
+時報を司るもじゅーる
+"""
+import datetime
+import asyncio
+
+import discord
+
+from lib.util import Singleton
+from lib.weather import get_weather
+
+
+class TimeSignal(Singleton):
+    """
+    時報を操作するClass
+    """
+    def __init__(self, channel, messages, timezone=9, report_timing=(6, 19)):
+        """
+        時報に使用する変数の初期化
+        Parameters
+        ----------
+        channel: discord.Channel
+            時報を送信するチャンネル
+        messages: dict<int, str>
+            時報で送信するメッセージを指定する
+        timezone: int
+            default = UTC+9
+            時報で利用するタイムゾーンを指定する
+        """
+        self.channel = channel
+        self.messages = messages
+        self.timezone = datetime.timezone(datetime.timedelta(hours=timezone))
+
+        self.weather_report_timing = {
+            "today": report_timing[0],
+            "tomorrow": report_timing[1]
+        }
+
+    async def base(self):
+        """
+        時報のループ処理を行う関数
+        """
+        while True:
+            time = datetime.datetime.now(tz=self.timezone)
+
+            if time.minute == 0:
+                await self.time_signal(time.hour)
+
+            await asyncio.sleep(50)
+
+    async def time_signal(self, hour):
+        """
+        時報を実行する関数
+        Parameters
+        ----------
+        hour: int
+            現在の時間
+        """
+        await self.channel.send(self.messages[hour])
+
+        if hour in self.weather_report_timing.values():
+            day = [d for d, v in self.weather_report_timing.items() if v == hour]
+
+            weather = get_weather()[day]
+
+            if day == "today":
+                await self.report_today(weather)
+            else:
+                await self.report_tomorrow(weather)
+
+        await asyncio.sleep(15)
+
+    async def report_today(self, weather):
+        """
+        今日の天気予報
+        """
+        await self.channel.send(
+            "今日の天気は{}\n最高気温は{}℃で昨日と{}℃違うよ\n最低気温は{}℃で昨日と{}℃違うよ\n今日も頑張ってね"\
+            .format(
+                weather["weather"],
+                weather["high"],
+                weather["high_diff"][1:-1],
+                weather["low"],
+                weather["low_diff"][1:-1]
+            )
+        )
+
+    async def report_tomorrow(self, weather):
+        """
+        昨日の天気予報
+        """
+        await self.channel.send(
+            "昨日の天気は{}\n最高気温は{}℃で今日と{}℃違うよ\n最低気温は{}℃で今日と{}℃違うよ\n今日も一日お疲れ様"\
+            .format(
+                weather["weather"],
+                weather["high"],
+                weather["high_diff"][1:-1],
+                weather["low"],
+                weather["low_diff"][1:-1]
+            )
+        )
+
--- main.py
+++ main.py
@@ -1,31 +1,29 @@
-import discord
-import datetime
+"""
+はらちょの根幹ファイル
+"""
 import asyncio
 import sys
 import os
-import subprocess
-import requests
 import json
 import codecs
-from random import randint
 
-from lib import scraping
-from lib.weather import get_weather
+import discord
+
 from lib.util import Singleton
 from lib.lol_counter import LolCounter
+from lib.time_signal import TimeSignal
 
 from message_command import MessageCommands
 
-import re
 
 class MainClient(discord.Client, Singleton):
     """
     Discordクライアント(多重起動防止機構付き)
     """
     CLI_BOTS = [
-        685429240908218368, 
-        684655652182032404, 
-        685457071906619505        
+        685429240908218368,
+        684655652182032404,
+        685457071906619505
     ]
     def __init__(self, token, base_channel_id):
         """
@@ -45,16 +43,17 @@ class MainClient(discord.Client, Singleton):
         # Initialize in on_ready()
         # Because use value in client
         self.base_channel = None
-        self.lol_counter  = None
-        self.HARASYO      = None
 
         # mesm_json_syntax_conceal = 0sages.json (時報json) の読み込みを試みる
         # msg_dictのkeyはstr型です、int型で呼び出そうとしないで()
-        with codecs.open("messages.json", 'r', 'utf-8') as f:
-            self.msg_dict = json.loads(f.read())
+        with codecs.open("messages.json", 'r', 'utf-8') as json_file:
+            self.msg_dict = json.loads(json_file.read())
 
 
     def launch(self):
+        """
+        clientの起動
+        """
         self.run(self.token)
 
     async def on_ready(self):
@@ -65,13 +64,23 @@ class MainClient(discord.Client, Singleton):
         if len(self.guilds) == 1:
             self.base_channel = self.get_channel(self.base_channel_id)
             self.lol_counter = LolCounter(self.guilds[0].members)
-            asyncio.ensure_future(self.ziho())
+
+            time_signal = TimeSignal(
+                self.base_channel,
+                self.msg_dict["ziho"]
+            )
+
+            asyncio.ensure_future(time_signal.base())
+
             harasyo = self.get_emoji(684424533997912096)
             isso = self.get_emoji(685162743317266645)
             MessageCommands.static_init(self.guilds[0].members, harasyo, isso)
             await self.base_channel.send("響だよ。その活躍ぶりから不死鳥の通り名もあるよ")
 
     async def on_message(self, message):
+        """
+        BOT以外がメッセージを送信したときに関数に処理をさせる
+        """
         if message.author.bot and not message.author.id in MainClient.CLI_BOTS:
             return
         channel = message.channel
@@ -79,47 +88,10 @@ class MainClient(discord.Client, Singleton):
         command = MessageCommands(message_str, channel, message)
         await command.execute()
 
-    async def ziho(self):
-        """
-        時報を制御する関数
-        """
-        timezone = datetime.timezone(datetime.timedelta(hours=9))
-        messages = self.msg_dict["ziho"]
-        while True:
-            time = datetime.datetime.now(tz=timezone)
-            if time.minute == 0:
-                hour = str(time.hour)
-                await self.base_channel.send(messages[hour])
-                if hour == "6":
-                    weather = get_weather()["today"]
-                    await self.base_channel.send(
-                        "今日の天気は{}\n最高気温は{}℃で昨日と{}℃違うよ\n最低気温は{}℃で昨日と{}℃違うよ\n今日も頑張ってね"\
-                        .format(
-                            weather["weather"],
-                            weather["high"],
-                            weather["high_diff"][1:-1],
-                            weather["low"],
-                            weather["low_diff"][1:-1]
-                        )
-                    )
-                if hour == "19":
-                    weather = get_weather()["tomorrow"]
-                    await self.base_channel.send(
-                        "明日の天気は{}\n最高気温は{}℃で今日と{}℃違うよ\n最低気温は{}℃で今日と{}℃違うよ\n今日も1日お疲れ様"\
-                        .format(
-                            weather["weather"],
-                            weather["high"],
-                            weather["high_diff"][1:-1],
-                            weather["low"],
-                            weather["low_diff"][1:-1]
-                        )
-                    )
-                await asyncio.sleep(15)
-            await asyncio.sleep(50)
 
 if __name__ == "__main__":
-    token = os.environ["TOKEN"]
-    base_channel_id = 684289417682223150
-    main = MainClient()
-    main.launch()
+    TOKEN = os.environ["TOKEN"]
+    BASE_CHANNEL = sys.argv[1]
+    MAIN = MainClient()
+    MAIN.launch()
 
