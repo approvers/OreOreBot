594897e122bd7105c114de7dd8e3457bcc000610
Colk-tech <colk.tech@gmail.com>
2020-04-08 10:39:10 +0900

    datetime.time型導入、#93修正、時間の指定方法変更

--- lib/kaere.py
+++ lib/kaere.py
@@ -10,25 +10,27 @@ from lib.util import Singleton
 
 class Kaere(Singleton):
     """
-    Kaereを司るクラス
+    !kaere関連の処理を司るクラス
     """
 
     def __init__(self, voice_channel, text_channel, hakaba_voice_channel):
         """
-        初期化処理
+        初期化処理を行う
         Parameters
         ----------
-        main_voice_channel: discord.TextChannel
-            メインのボイスチャンネルkaereが実行されるチャンネル
-        textn_channel: discord.VoiceChannel
-            聞き専チャンネルkaereのメッセージが送信されるチャンネル
+        voice_channel: discord.VoiceChannel
+            メインのボイスチャンネルkaereが実行される
+        text_channel: discord.TextChannel
+            聞き専チャンネルkaereに関するメッセージが送信される
+        hakaba_voice_channel: discord.VoiceChannel
+            墓場のボイスチャンネル 設定によって実行後ここに強制移動させられる
         """
-        self.timezone       = datetime.timezone(datetime.timedelta(hours=9))
-        self.voice_channel  = voice_channel
-        self.text_channel   = text_channel
+        self.timezone = datetime.timezone(datetime.timedelta(hours=9))
+        self.voice_channel = voice_channel
+        self.text_channel = text_channel
         self.hakaba_voice_channel = hakaba_voice_channel
 
-        self.kaere_do_dict = {} # {str:discord.User}
+        self.kaere_do_dict = {}  # {str:discord.User.display_name}
         self.do_disconnect = False
         self.is_doing = False
 
@@ -41,15 +43,15 @@ class Kaere(Singleton):
 
         if commands[0] == "set":
             if len(commands) < 2:
-                await self.text_channel.send("お知らせしてほしい時間を`!kaere set HHMM`の形式でおしえてね")
+                await self.text_channel.send("お知らせしてほしい時間を`!kaere set HH:MM`の形式でおしえてね")
                 return
-            if not await self.time_valid(commands[1]):
+            if not await self.time_controller(commands[1]):
                 return
-            if commands[1] in self.kaere_do_dict:
+            if datetime.datetime.strptime(commands[1], "%H:%M").time() in self.kaere_do_dict:
                 await self.text_channel.send("その時間はもうお知らせする予定だよ")
                 return
-            await self.text_channel.send("{}におしらせするね".format(commands[1]))
-            self.kaere_do_dict[commands[1]] = member_name
+            await self.text_channel.send("{}におしらせするね".format(str(datetime.datetime.strptime(commands[1], "%H:%M").time())[0:5]))
+            self.kaere_do_dict[datetime.datetime.strptime(commands[1], "%H:%M").time()] = member_name
             return
 
         if commands[0] == "list":
@@ -58,22 +60,21 @@ class Kaere(Singleton):
                 return
             display_text = "***蛍の光予約一覧***\n"
             for do_time, member_in in self.kaere_do_dict.items():
-                print(do_time)
-                display_text += ("***・{}時{}分*** ({})\n").format(do_time[0:2], do_time[2:4], member_in)
+                display_text += ("***・{}*** ({})\n").format(str(do_time)[0:5], member_in)
             await self.text_channel.send("お知らせする時間のリストだよ\n" + display_text)
             return
 
         if commands[0] == "remove":
             if len(commands) < 2:
-                await self.text_channel.send("お知らせをキャンセルしたい時間を`!kaere remove HHMM`の形式でおしえてね")
+                await self.text_channel.send("お知らせをキャンセルしたい時間を`!kaere remove HH:MM`の形式でおしえてね")
                 return
-            if not await self.time_valid(commands[1]):
+            if not await self.time_controller(commands[1]):
                 return
-            if not commands[1] in self.kaere_do_dict.keys():
+            if not (datetime.datetime.strptime(commands[1], "%H:%M").time() in self.kaere_do_dict):
                 await self.text_channel.send("その時間にお知らせする予定はないよ")
                 return
-            self.kaere_do_dict.pop(commands[1])
-            await self.text_channel.send("{}のお知らせをキャンセルしたよ".format(commands[1]))
+            self.kaere_do_dict.pop(datetime.datetime.strptime(commands[1], "%H:%M").time())
+            await self.text_channel.send("{}のお知らせをキャンセルしたよ".format(str(datetime.datetime.strptime(commands[1], "%H:%M").time())[0:5]))
             return
 
         if commands[0] == "force":
@@ -84,23 +85,21 @@ class Kaere(Singleton):
                 self.do_disconnect = True
                 await self.text_channel.send("強制切断をオンにしたよ")
 
-    async def time_valid(self,check_str):
+    async def time_controller(self, checker):
         try:
-            is_ok = ((0 <= int(check_str[0:2]) <= 23) and (0 <= int(check_str[2:4]) < 60)) and (len(check_str) == 4)
-            if not is_ok:
-                await self.text_channel.send("そんな時間は存在しないよ...？")
-                return False
+            datetime.datetime.strptime(checker, "%H:%M").time()
+            return True
         except:
             await self.text_channel.send("そんな時間は存在しないよ...？")
             return False
-        return True
 
     async def base(self):
         """
-        ゲリラのループ処理を行う関数
+        蛍の光のループ処理を行う関数
         """
         while True:
-            time = datetime.datetime.now(tz=self.timezone).strftime("%H%M")
+            time = datetime.datetime.now(tz=self.timezone)
+            time = datetime.time(hour=time.hour, minute=time.minute)
             if time in self.kaere_do_dict.keys():
                 self.kaere_do_dict.pop(time)
                 await self.do(is_not_list=True)
@@ -119,8 +118,9 @@ class Kaere(Singleton):
             await self.text_channel.send("アナウンスの終了後、強制切断するナリ")
         await asyncio.sleep(100)
         if self.do_disconnect and is_not_list:
+            await voice_client.disconnect(force=True)
+            await asyncio.sleep(0.50)
             for member in self.voice_channel.members:
                 await member.move_to(channel=self.hakaba_voice_channel, reason="†***R.I.P.***† ***安らかに眠れ***")
                 await asyncio.sleep(0.50)
-        await voice_client.disconnect(force=True)
         self.is_doing = False
