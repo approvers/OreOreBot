2d65486e95de3adf3214307edd305eb5d075eddd
isso0424 <isso04240129@gmail.com>
2020-04-10 02:44:07 +0900

    [Fix]ウンコードちね+kaere中断でバグらないように修正した

--- lib/kaere.py
+++ lib/kaere.py
@@ -30,69 +30,124 @@ class Kaere(Singleton):
         self.text_channel = text_channel
         self.hakaba_voice_channel = hakaba_voice_channel
 
-        self.kaere_do_dict = {}  # {str:discord.User.display_name}
+        self.kaere_schedule = {}  # {str:discord.User.display_name}
         self.do_disconnect = False
         self.is_doing = False
 
     async def command_controller(self, commands, member_name):
+        """
+        !kaereの根幹部分です
+        Param
+        -----
+        commands: list[str]
+            ユーザーが入力した文字列を半角スペースで分割したものが入ります
+        member_name: str
+            ユーザーの名前が入ります
+        """
         commands.pop(0)
 
         if len(commands) == 0:
-            await self.do(is_not_list=False)
+            await self.execute(is_auto=False)
             return
 
-        if commands[0] == "set":
+        command = commands[0]
+
+        if command == "set":
             if len(commands) < 2:
                 await self.text_channel.send("お知らせしてほしい時間を`!kaere set HH:MM`の形式でおしえてね")
                 return
-            if not await self.time_controller(commands[1]):
-                return
-            if datetime.datetime.strptime(commands[1], "%H:%M").time() in self.kaere_do_dict:
-                await self.text_channel.send("その時間はもうお知らせする予定だよ")
-                return
-            await self.text_channel.send("{}におしらせするね".format(str(datetime.datetime.strptime(commands[1], "%H:%M").time())[0:5]))
-            self.kaere_do_dict[datetime.datetime.strptime(commands[1], "%H:%M").time()] = member_name
+
+            time = commands[1]
+            await self.kaere_set(time, member_name)
             return
 
-        if commands[0] == "list":
-            if len(self.kaere_do_dict) == 0:
-                await self.text_channel.send("まだお知らせする予定はないよ")
-                return
-            display_text = "***蛍の光予約一覧***\n"
-            sorted_dict = sorted(self.kaere_do_dict.items(), key=lambda a: a[0])
-            for do_time, member_in in sorted_dict:
-                display_text += ("***・{}*** ({})\n").format(str(do_time)[0:5], member_in)
-            await self.text_channel.send("お知らせする時間のリストだよ\n" + display_text)
+        if command == "list":
+            await self.scheduled_kaere_list()
             return
 
-        if commands[0] == "remove":
+        if command == "remove":
             if len(commands) < 2:
                 await self.text_channel.send("お知らせをキャンセルしたい時間を`!kaere remove HH:MM`の形式でおしえてね")
                 return
-            if not await self.time_controller(commands[1]):
-                return
-            if not (datetime.datetime.strptime(commands[1], "%H:%M").time() in self.kaere_do_dict):
-                await self.text_channel.send("その時間にお知らせする予定はないよ")
-                return
-            self.kaere_do_dict.pop(datetime.datetime.strptime(commands[1], "%H:%M").time())
-            await self.text_channel.send("{}のお知らせをキャンセルしたよ".format(str(datetime.datetime.strptime(commands[1], "%H:%M").time())[0:5]))
+            time = commands[1]
+            await self.schedule_time_remove(time)
             return
 
-        if commands[0] == "force":
+        if command == "force":
             if self.do_disconnect:
-                self.do_disconnect = False
                 await self.text_channel.send("強制切断をオフにしたよ")
             else:
-                self.do_disconnect = True
                 await self.text_channel.send("強制切断をオンにしたよ")
+            self.do_disconnect = not self.do_disconnect
 
-    async def time_controller(self, checker):
+    async def schedule_time_remove(self, raw_time):
+        """
+        予定されている!kaereをキャンセルする
+        """
         try:
-            datetime.datetime.strptime(checker, "%H:%M").time()
-            return True
-        except:
+            time = self.str_to_time(raw_time)
+        except ValueError:
             await self.text_channel.send("そんな時間は存在しないよ...？")
-            return False
+            return
+
+        if time in self.kaere_schedule:
+            self.kaere_schedule.pop(time)
+            await self.text_channel.send("{}のお知らせをキャンセルしたよ".format(str(time)[0:5]))
+            return
+
+        await self.text_channel.send("その時間にお知らせする予定はないよ")
+
+    async def scheduled_kaere_list(self):
+        """
+        予定されている!kaereのリストを送信する
+        """
+        if len(self.kaere_schedule) == 0:
+            await self.text_channel.send("まだお知らせする予定はないよ")
+            return
+
+        display_text = "***蛍の光予約一覧***\n"
+        sorted_dict = sorted(self.kaere_schedule.items(), key=lambda a: a[0])
+
+        for schedule_time, member_name in sorted_dict:
+            display_text += ("***・{}*** ({})\n").format(str(schedule_time)[0:5], member_name)
+        await self.text_channel.send("お知らせする時間のリストだよ\n" + display_text)
+
+    async def kaere_set(self, raw_time, member_name):
+        """
+        !kaereを予約する
+        Param
+        -----
+        raw_time: str
+            変換前の時間の文字列
+        member_name: str
+            リクエストしたメンバーの名前
+        """
+        try:
+            time = self.str_to_time(raw_time)
+        except ValueError:
+            await self.text_channel.send("そんな時間は存在しないよ...？")
+            return
+
+        if time in self.kaere_schedule:
+            await self.text_channel.send("その時間はもうお知らせする予定だよ")
+            return
+
+        await self.text_channel.send("{}におしらせするね".format(str(time)[0:5]))
+        self.kaere_schedule[time] = member_name
+
+    def str_to_time(self, time: str):
+        """
+        Param
+        -----
+        time: str
+            時間であるかチェックする文字列
+
+        Return
+        ------
+        time: datetime.time
+            時間のフォーマットに合っているかの真理値
+        """
+        return datetime.datetime.strptime(time, "%H:%M").time()
 
     async def base(self):
         """
@@ -101,33 +156,45 @@ class Kaere(Singleton):
         while True:
             time = datetime.datetime.now(tz=self.timezone)
             time = datetime.time(hour=time.hour, minute=time.minute)
-            if time in self.kaere_do_dict.keys():
-                self.kaere_do_dict.pop(time)
-                await self.do(is_not_list=True)
+            if time in self.kaere_schedule.keys():
+                self.kaere_schedule.pop(time)
+                await self.execute(is_auto=True)
             await asyncio.sleep(50)
 
-    async def do(self,is_not_list):
+    async def execute(self, is_auto):
         """
         実際にkaereを実行する
         """
         if self.is_doing:
+            await self.text_channel.send("もう実行されてるよ")
             return
         try:
             self.is_doing = True
             voice_client = await self.voice_channel.connect(reconnect=False)
             voice_client.play(discord.FFmpegPCMAudio(source="ast/snd/neroyo.mp3"))
-            if self.do_disconnect and is_not_list:
+            if self.do_disconnect and is_auto:
                 await self.text_channel.send("アナウンスの終了後、強制切断するナリ")
-            await asyncio.sleep(100)
-            if self.do_disconnect and is_not_list:
+
+            for _ in range(20):
+                if not voice_client.is_connected():
+                    self.is_doing = False
+                    await voice_client.disconnect(force=True)
+                    break
+                await asyncio.sleep(5)
+
+            if self.do_disconnect and is_auto:
                 await voice_client.disconnect(force=True)
                 await asyncio.sleep(0.50)
                 for member in self.voice_channel.members:
-                    await member.move_to(channel=self.hakaba_voice_channel, reason="†***R.I.P.***† ***安らかに眠れ***")
+                    await member.move_to(
+                        channel=self.hakaba_voice_channel,
+                        reason="†***R.I.P.***† ***安らかに眠れ***"
+                    )
                     await asyncio.sleep(0.50)
         except discord.errors.ClientException:
-            await self.text_channel.send("もう実行されてるぞカス")
-        except:
-            await self.text_channel.send("不明な例外が発生したぞカス")
-            self.is_doing = False
+            await self.text_channel.send("もう実行されてるよ")
+        except Exception as e:
+            await self.text_channel.send("例外が発生したよ\n内容は{}だよ".format(e))
+
         self.is_doing = False
+
