64fd11c11100d646f5f3ea1200a41daf44115289
Colk-tech <colk.tech@gmail.com>
2020-04-13 20:37:31 +0900

    何したか忘れた(認知症)

--- lib/editmiteta.py
+++ lib/editmiteta.py
@@ -7,7 +7,7 @@ import difflib
 import discord
 
 
-async def mitetazo_edit(before, after):
+async def mitetazo_edit(before: discord.Message, after: discord.Message):
     """
     実際に編集を感知したときに呼ばれる関数
     編集前のメッセージを送信します
@@ -18,15 +18,15 @@ async def mitetazo_edit(before, after):
     after: discord.Message
         変更後のMessageオブジェクト
     """
-    before_text = before.content
-    after_text = after.content
+    before_text = before.content.split("\n")
+    after_text = after.content.split("\n")
     composed_text = diff_composer(before_text, after_text)
     if composed_text == "":
         return
     await after.channel.send("見てたぞ\n```\n{}\n```".format(composed_text))
 
 
-def diff_composer(before_text, after_text):
+def diff_composer(before_text: list, after_text: list):
     """
     beforeとafterのテキストを比較して
     結果に - + が含まれる行のみをstrで返しますね
@@ -42,7 +42,7 @@ def diff_composer(before_text, after_text):
         改行なども含め処理をした最終成果物
     """
     diff_object = difflib.Differ()
-    diff = diff_object.compare(before_text.split("\n"), after_text.split("\n"))
+    diff = diff_object.compare(before_text, after_text)
     diff_list = list(diff)
     diff_text_list = [i for i in diff_list if i.startswith("+") or i.startswith("-")]
     return "\n".join(diff_text_list)
