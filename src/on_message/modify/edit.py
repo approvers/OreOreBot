7e20bef93a7233cd04a98533699d0904a2cde210
loxygenk <loxygen.k@gmail.com>
2020-06-08 20:42:53 +0900

    [Edit] 関数型でいいだろこれ

--- src/on_message/modify/edit.py
+++ src/on_message/modify/edit.py
@@ -1,56 +1,44 @@
 import difflib
-<<<<<<< HEAD
 import discord
-=======
->>>>>>> 986e4cfffd032159806106082717090a0746078d
 from typing import List
 
 
-class EditNotifier:
-    MESSAGE_TEMPLATE = "見てたぞ\n```diff\n{}```"
-    DIFF_PAIR_TEMPLATE = "{}\n{}\n{}"
-    HORIZONTAL_LINE = "------------------------------\n"
+MESSAGE_TEMPLATE = "見てたぞ\n```diff\n{}```"
+DIFF_PAIR_TEMPLATE = "{}\n{}\n{}"
+HORIZONTAL_LINE = "------------------------------\n"
 
-    def notify(
-        self,
-<<<<<<< HEAD
-        before: discord.Message,
-        after: discord.Message,
-    ):
-        before_lines = before.content.split("\n")
-        after_lines = after.content.split("\n")
-=======
-        before: str,
-        after: str,
-    ):
-        before_lines = before.split("\n")
-        after_lines = after.split("\n")
->>>>>>> 986e4cfffd032159806106082717090a0746078d
 
-        diff_str = EditNotifier.generateDiff(
-            before_lines, after_lines
+def edit_notify(
+    self,
+    before: discord.Message,
+    after: discord.Message,
+):
+    before_lines = before.content.split("\n")
+    after_lines = after.content.split("\n")
+    diff_str = generateDiff(
+        before_lines, after_lines
+    )
+    if diff_str == "":
+        return
+
+    await after.channel.send(
+        MESSAGE_TEMPLATE.format(diff_str)
+    )
+
+
+def generateDiff(
+    before_lines: List[str],
+    after_lines: List[str]
+):
+    raw_diff = list(difflib.Differ().compare(before_lines, after_lines))
+    modified_diff = [x for x in raw_diff if x[:1] in "+-"]
+
+    generated_diff_text = ""
+    for i in range(0, len(modified_diff), 2):
+        generated_diff_text += DIFF_PAIR_TEMPLATE.format(
+            modified_diff[i],
+            modified_diff[i + 1],
+            HORIZONTAL_LINE if (i + 1) != len(modified_diff) else ""
         )
-        if diff_str == "":
-            return
 
-        await after.channel.send(
-            EditNotifier.MESSAGE_TEMPLATE.format(diff_str)
-        )
-
-    @staticmethod
-    def generateDiff(
-        before_lines: List[str],
-        after_lines: List[str]
-    ):
-        raw_diff = list(difflib.Differ().compare(before_lines, after_lines))
-        modified_diff = [x for x in raw_diff if x[:1] in "+-"]
-
-        generated_diff_text = ""
-        for i in range(0, len(modified_diff), 2):
-            generated_diff_text += EditNotifier.DIFF_PAIR_TEMPLATE.format(
-                modified_diff[i],
-                modified_diff[i + 1],
-                EditNotifier.HORIZONTAL_LINE if (i + 1) != len(modified_diff) else ""
-            )
-
-        return generated_diff_text.strip()
+    return generated_diff_text.strip()
