dd0dfcf014031f86322806eca89e363bb2a37061
loxygenk <loxygen.k@gmail.com>
2020-06-08 19:26:37 +0900

    [Add] メッセージ編集時の通知を司るクラスを作った

---
+++ src/on_message/modify/edit.py
@@ -0,0 +1,44 @@
+import difflib
+from typing import List
+
+
+class EditNotifier:
+    MESSAGE_TEMPLATE = "見てたぞ\n```diff\n{}```"
+    DIFF_PAIR_TEMPLATE = "{}\n{}\n{}"
+    HORIZONTAL_LINE = "------------------------------\n"
+
+    def notify(
+        self,
+        before: str,
+        after: str,
+    ):
+        before_lines = before.split("\n")
+        after_lines = after.split("\n")
+
+        diff_str = EditNotifier.generateDiff(
+            before_lines, after_lines
+        )
+        if diff_str == "":
+            return
+
+        await after.channel.send(
+            EditNotifier.MESSAGE_TEMPLATE.format(diff_str)
+        )
+
+    @staticmethod
+    def generateDiff(
+        before_lines: List[str],
+        after_lines: List[str]
+    ):
+        raw_diff = list(difflib.Differ().compare(before_lines, after_lines))
+        modified_diff = [x for x in raw_diff if x[:1] in "+-"]
+
+        generated_diff_text = ""
+        for i in range(0, len(modified_diff), 2):
+            generated_diff_text += EditNotifier.DIFF_PAIR_TEMPLATE.format(
+                modified_diff[i],
+                modified_diff[i + 1],
+                EditNotifier.HORIZONTAL_LINE if (i + 1) != len(modified_diff) else ""
+            )
+
+        return generated_diff_text.strip()
