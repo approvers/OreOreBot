0dfa3cea416ed6b266f4bb02256944d54925e6dd
loxygenk <loxygen.k@gmail.com>
2020-06-17 00:48:58 +0900

    [Fix] 命名カス

--- src/on_role_created/role.py
+++ src/on_role_created/role.py
@@ -5,9 +5,9 @@ class KawaemonRoleAdder:
 
     ROLE_ADD_MESSAGE_TEMPLATE = "{}ですが、{}にも追加しておきました"
 
-    def __init__(self, kawae_user_id: discord.Member, announce_text_channel_id: discord.TextChannel):
-        self.kawae_member: discord.Member = kawae_user_id
-        self.notify_channel: discord.TextChannel = announce_text_channel_id
+    def __init__(self, kawae_member: discord.Member, announce_text_channel: discord.TextChannel):
+        self.kawae_member: discord.Member = kawae_member
+        self.notify_channel: discord.TextChannel = announce_text_channel
 
     async def add_role(
         self,
