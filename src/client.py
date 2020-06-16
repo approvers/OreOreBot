4e7211401f648248d2b45bd7509ca96b4190a8ed
flisan <55672846+loxygenK@users.noreply.github.com>
2020-06-17 01:00:20 +0900

    Addrole to kawae (#138)
    * [Add] ロール追加時の関数を追加した
    
    * [Add] 実際にかわえにロールを追加する処理を書いた
    
    * [Fix] ガバやめ
    
    * [Fix] 最初からクラスにしけおけばこんなことにはならんかったのに…
    
    * [Fix] various
    
    * [Fix] 命名カス

--- src/client.py
+++ src/client.py
@@ -6,7 +6,7 @@ from src.config.load import load_config
 from src.voice_state_update.change_voice_state import VoiceStateNotifier
 from src.message_edit.edit import notify_message_edit
 from src.message_delete.delete import notify_message_delete
-
+from src.on_role_created.role import KawaemonRoleAdder
 
 class MainClient(discord.Client):
     def __init__(self) -> None:
@@ -22,6 +22,10 @@ class MainClient(discord.Client):
         self.message_manager = MessageRoot(
             self,
             config
+        )
+        self.kawae_role_adder = KawaemonRoleAdder(
+            self.get_user(config["role"]["user_id"]),
+            self.get_channel(config["text_channel"]["base"])
         )
         self.voice_state_notifier = VoiceStateNotifier()
 
@@ -53,5 +57,4 @@ class MainClient(discord.Client):
         await notify_message_delete(message)
 
     async def on_guild_role_create(self, role):
-        pass
-
+        await self.kawae_role_adder.add_role(role)
