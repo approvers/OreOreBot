f7b2e14a23fa5cf8d08409c48dc314b86310f859
loxygenk <loxygen.k@gmail.com>
2020-06-16 15:33:54 +0900

    [Fix] ガバやめ

--- src/on_role_created/role.py
+++ src/on_role_created/role.py
@@ -6,8 +6,8 @@ async def add_role_to_kawae(
     config: Dict[str, Dict[str, Union[str, int]]],
     role: discord.Role
 ):
-    user: discord.Member = role.guild.get_member(config["role"]["user_id"])
-    notify_channel: discord.TextChannel = role.guild.get_channel(config["role"]["user_id"])
+    user: discord.Member = role.guild.get_member(config["add_role"]["user_id"])
+    notify_channel: discord.TextChannel = role.guild.get_channel(config["text_channel"]["base"])
     await user.add_roles(role)
     await notify_channel.send(role.mention + "ですが、かわえにも追加しておきました")
 
